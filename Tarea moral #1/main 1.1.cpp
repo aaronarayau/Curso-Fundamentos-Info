d14a7dcf5a42d9a3f0cc5699aa2d96b347f5ad8d
aaronarayau <aaronus421@gmail.com>
2026-03-01 20:02:59 -0600

    Tarea Moral #1 Completa

---
+++ Tarea moral #1/main 1.1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	
+	// Cajas de bienvenida!
+	
+	cout << string(19, '*') ;
+	cout << "\t "<<string(19, '=') ;
+	cout << "\t "<< "+"<< string (9, '-') <<"+"<<endl;
+	
+	cout << "*" << string(17, ' ') << "*" ;
+	cout << "\t " << "||" << string(15, ' ') << "||" ;
+	cout << "\t " << "|"<< string(9, ' ') << "|" << endl;
+	
+	cout << "*" << string(3, ' ') <<"Bienvenido!" << string(3, ' ') << "*" ;
+	cout << "\t " << "||" << string(2, ' ') << "Bienvenido!" << string(2, ' ') <<"||";
+	cout << "\t" << " |" << "  Hola!  "<< "|"<<endl;
+	
+	cout << "*" << string(17, ' ') << "*";
+	cout << "\t "<<"||" << string(15, ' ') << "||";
+	cout << "\t " << "|"<< string(9, ' ') << "|" << endl;
+	
+	cout << string(19, '*') ;
+	cout << "\t " <<string (19, '='); 
+	cout << "\t " <<"+"<<string (9, '-') <<"+"<< endl;
+	
+	cout <<"\n \n";
+		
+	// Figuras
+	cout <<"   *"<< "\t  " << "****" << "\t  " << "****" << endl;
+	cout <<"  **"<< "\t  " << "*** " << "\t  " << "****" << endl;
+	cout <<" ***"<< "\t  " << "**  " << "\t  " << "****" << endl;
+	cout <<"****"<< "\t  "<<  "*   " << "\t  " << "****" << endl;
+	return 0;
+}
