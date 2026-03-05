bbb0b07b01b42c87c743a99ceb85f23224725b05
aaronarayau <aaronus421@gmail.com>
2026-03-05 10:29:22 -0600

    Calculadoras

---
+++ Calculadora Funcional/main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	
+	//Codigo base 1-10 de la disfuncional
+	
+	switch (opcion){
+		
+	case 1:
+		//haga la suma
+		break;
+	case 2:
+		//haga la resta
+		cout << "resta =" << a-b;
+		break;  //Break sale del switch
+		default :
+			// cout << "Intente de nuevo" << endl;
+			
+	case 4:
+		
+		if(b == 0){
+			cout << "Intente de nuevo" << endl;
+			return 0;
+		}else{
+			cout<<"division =" << float(a)/b;
+		};
+	}
+	
+	return 0;
+}
+
